5103bc95b4e333f5bb69ecd5f853ae50c0259a29
tarjoCosmar <tarjo_cm@hotmail.com>
2024-08-25 11:52:34 -0500

    reaciendo carrusel

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal', // Cambia a 'vertical' si prefieres
+    loop: true, // Para que el carrusel sea infinito
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
