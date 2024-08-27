529fd8c16aa93d8925901d8e67c52321256710b6
tarjoCosmar <tarjo_cm@hotmail.com>
2024-08-26 21:24:58 -0500

    reeeeeeeel

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 10,
-    spaceBetween: 80,
-    grabCursor: true,
+new Swiper(".swiper-container", {
+    direction: 'horizontal',
     loop: true,
 
-});
-
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+        },
+        
+    autoplay: {
+        delay: 2000,
+    },
 });
